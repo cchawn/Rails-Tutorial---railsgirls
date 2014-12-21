f8eac27fd5d416ec9dec8005c626b448ee33ec53
Christina Chan <christinaavchan@gmail.com>
2014-12-20 20:30:50 -0500

    Adding User Authentication

--- railsgirls/config/routes.rb
+++ railsgirls/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/info'
+
+  root :to => redirect('/ideas')
   resources :ideas
 
   # The priority is based upon order of creation: first created -> highest priority.
